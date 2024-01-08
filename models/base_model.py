ac76d990ba5328b53499d9eb45e8617fba985286
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-08 21:01:46 +0100

    Changed base code to accept keyword args(dict)

--- models/base_model.py
+++ models/base_model.py
@@ -4,19 +4,26 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.update_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            self.id = kwargs["id"]
+            self.created_at = datetime.datetime.fromisoformat(
+                kwargs["created_at"])
+            self.updated_at = datetime.datetime.fromisoformat(
+                kwargs["updated_at"])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.update_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        self.update_at = datetime.datetime.isoformat(self.update_at)
+        self.updated_at = datetime.datetime.isoformat(self.updated_at)
         self.created_at = datetime.datetime.isoformat(self.created_at)
         self.__dict__["__class__"] = __class__.__name__
         return (self.__dict__)
