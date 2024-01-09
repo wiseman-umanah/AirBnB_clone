1f12d028a39cb2b60e282383fefb81707b6308af
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-09 15:56:58 +0100

    Updated console engine

--- console.py
+++ console.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
 import cmd
 import sys
+from models import storage
 from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    intro = "AirBnB project, Type ? or help to list commands"
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        print("Bye 👋")
         sys.exit()
 
     def do_EOF(self, arg):
         """This function will close the program after to indicate EOF"""
-        print("Bye 👋")
         sys.exit()
 
     def do_help(self, arg):
         """Get help on a specific command."""
         cmd.Cmd.do_help(self, arg)
-
+	
     def do_create(self, arg):
         if arg == "":
             print("** class name missing **")
+        elif arg != "BaseModel":
+            print("** class doesn't exist **")
         else:
             arg = BaseModel()
             print(arg.id)
+            arg.save()
 
 
 if __name__ == '__main__':
