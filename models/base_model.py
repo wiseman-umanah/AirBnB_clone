3404cd62fa5985c57f87bb046a1848c716731771
wiseman-umanah <wisemanumanah@gmail.com>
2024-01-10 18:17:39 +0100

    Added user class and updated file storage

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
             self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
 from models.base_model import BaseModel
+from models.user import User
 import json
 
 
